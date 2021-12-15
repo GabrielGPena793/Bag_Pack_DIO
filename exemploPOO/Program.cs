5ca14f37952dbb04f5785984c4255b3aab7fb7f6
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2021-12-15 15:03:25 -0300

    abstraçao

---
+++ exemploPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using exemploPOO.model;
+
+namespace exemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome =  "Gabriel";
+            p1.Idade = 25;
+
+            p1.Apresentar();
+        }
+    }
+}
---
+++ exemploPOO/model/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace exemploPOO.model
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é {this.Nome}, e minha idade é {this.Idade} anos");
+        }
+    }
+}
