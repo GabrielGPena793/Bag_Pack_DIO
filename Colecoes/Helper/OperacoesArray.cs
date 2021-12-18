2a4d26b65d216b8eeef15c23cf125f4aa1b29693
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2021-12-17 22:26:49 -0300

    manipulando Array

---
+++ Colecoes/Helper/OperacoesArray.cs
@@ -0,0 +1,70 @@
+namespace Colecoes.Helper
+{
+    public class OperacoesArray
+    {
+
+
+        public void OrdernarBubbleSort(ref int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j + 1])                 // se o elemento atual for maior que o proximo
+                    {
+                        temp = array[j +1];                     // pega o valor da proxima posição e guarda numa variavel
+                        array[j +1] = array[j];                 // adiciona o valor da posição anterior na proxima posição
+                        array[j] = temp;                        //  pega o valor da variavel e coloca na posição anterior
+                    }
+                }
+            }
+        }
+        public void ImprimirArray(int[] array)
+        {
+            string linha = string.Join(", ", array);            // separando cada posição do meu array por virgula e unindo em uma string
+            System.Console.WriteLine(linha);
+        }
+
+        public void Ordenar(ref int[] array)
+        {
+            Array.Sort(array);
+        }
+
+        public void CopiarArray(ref int[] array, ref int[] arrayDestino)
+        {
+            Array.Copy(array, arrayDestino, array.Length);
+        }
+
+        public bool Existe(int[] array, int valor)
+        {
+            return Array.Exists(array, element => element == valor);        // verificando se existe um valor igual ao que estou procurando
+        }
+
+        public bool TodosMaiorQue(int[] array, int valor)
+        {
+            return Array.TrueForAll(array, element => element > valor);     // verificando se todos so valores são maiores ao valor que estou passando
+        }
+
+        public int ObterValor(int[] array, int valor)
+        {
+            return Array.Find(array, element => element == valor);          // Procura o valor e retorna a primeira ocorrência que achar, se não achar retorna 0
+        }
+
+        public int ObterIndice(int[] array, int valor)
+        {
+            return Array.IndexOf(array, valor);                             // retorna o indice do valor se achar, se não retorna -1
+        }
+
+        public void RedimencionarArray(ref int[] array, int valor)
+        {
+            Array.Resize(ref array, valor);                                 // mudando o tamanho do array já existente
+        }
+
+        public string[] convertArray(int[] array)
+        {
+            return Array.ConvertAll(array, element => element.ToString());
+        }
+    }
+}
--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,47 +1,125 @@
-﻿namespace Colecoes
+﻿using Colecoes.Helper;
+
+namespace Colecoes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            OperacoesArray operacoes = new OperacoesArray();
+            int[] numeros = new int[5] {10,2,8,1,6};
+            int[] numerosCopiados = new int[10];
+            string[] numerosConvertidos = operacoes.convertArray(numeros);
+
+
+            // int valorProcurado = 1;
+
+            // System.Console.WriteLine($"Array com tamanho antes do rezise {numeros.Length}");
+
+            // operacoes.RedimencionarArray(ref numeros, numeros.Length * 2);
+
+            // System.Console.WriteLine($"Array com tamanho depois do rezise {numeros.Length}");
+
+
+
+            // int indiceDoValor = operacoes.ObterIndice(numeros, valorProcurado);
+
+            // if(indiceDoValor >= 0)
+            // {
+            //     System.Console.WriteLine($" valor: {valorProcurado}, indice; {indiceDoValor}");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Valor não encontrado");
+            // }
+
+
+            // int valorAchado =  operacoes.ObterValor(numeros, valorProcurado);
+
+            // if(valorAchado > 0)
+            // {
+            //     System.Console.WriteLine($"Encontrei o valor {valorAchado}");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine($"Não tem o valor no array");
+
+            // }
+            // bool todosMaiorQue =  operacoes.TodosMaiorQue(numeros, valorProcurado);
+            // if(todosMaiorQue)
+            // {
+            //     System.Console.WriteLine($"Todos são maiores que o valor {valorProcurado}");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine($"Existem valores menores que {valorProcurado}");
+            // }
+
+            // bool existe = operacoes.Existe(numeros, valorProcurado);
+
+            // if(existe)
+            // {
+            //     System.Console.WriteLine($"O valor {valorProcurado} existe");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine($"O valor {valorProcurado} não existe");
+            // }
+
+            // System.Console.WriteLine("Imprimeinto array normal");
+            // operacoes.ImprimirArray(numeros);
+
+            // System.Console.WriteLine("Imprimindo array ordenado");
+            // // operacoes.OrdernarBubbleSort(ref numeros);
+            // operacoes.Ordenar(ref numeros);
+            // operacoes.ImprimirArray(numeros);
+
+            // System.Console.WriteLine("Antes da cópia");
+            // operacoes.ImprimirArray(numerosCopiados);
             
-            // int[] arryInteiros = new int[3];
-            // arryInteiros[0] = 10;
-            // arryInteiros[1] = 20;
-            // arryInteiros[2] = 30;
+            // operacoes.CopiarArray(ref numeros, ref numerosCopiados);
 
-            // System.Console.WriteLine("Pecorrendo o arry com for");
-            // for (int i = 0; i < arryInteiros.Length; i++)
-            // {
-            //     System.Console.WriteLine(arryInteiros[i]);
-            // }
-
-            // System.Console.WriteLine("Pecorrendo o arry com foreach");
-
-            // foreach (int valor in arryInteiros)
-            // {
-            //     System.Console.WriteLine(valor);
-            // }
-
-            int[,] matriz = new int[4,2]   //declarando a atribundo valores  uma matriz
-            {
-                {8,5},
-                {10,20},
-                {50,100},
-                {70,80}
-            };
-
-            // matriz[0, 0] = 5; também da pra atribuir colocando o número da linha e coluna
+            // System.Console.WriteLine("Depois da cópia");
+            // operacoes.ImprimirArray(numerosCopiados);
 
 
-            // pecorrendo uma matriz
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    System.Console.WriteLine(matriz[i, j]);
-                }
-            }
+        //     int[] arryInteiros = new int[3];
+        //     arryInteiros[0] = 10;
+        //     arryInteiros[1] = 20;
+        //     arryInteiros[2] = 30;
+
+        //     System.Console.WriteLine("Pecorrendo o arry com for");
+        //     for (int i = 0; i < arryInteiros.Length; i++)
+        //     {
+        //         System.Console.WriteLine(arryInteiros[i]);
+        //     }
+
+        //     System.Console.WriteLine("Pecorrendo o arry com foreach");
+
+        //     foreach (int valor in arryInteiros)
+        //     {
+        //         System.Console.WriteLine(valor);
+        //     }
+
+        //     int[,] matriz = new int[4,2]   //declarando a atribundo valores  uma matriz
+        //     {
+        //         {8,5},
+        //         {10,20},
+        //         {50,100},
+        //         {70,80}
+        //     };
+
+        //     // matriz[0, 0] = 5; também da pra atribuir colocando o número da linha e coluna
+
+
+        //     // pecorrendo uma matriz
+        //     for (int i = 0; i < matriz.GetLength(0); i++)
+        //     {
+        //         for (int j = 0; j < matriz.GetLength(1); j++)
+        //         {
+        //             System.Console.WriteLine(matriz[i, j]);
+        //         }
+        //     }
         }
     }
 }
